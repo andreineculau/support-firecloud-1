19c648c6510fce91662981f85f357c2e055fbfd7
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-03 07:17:56 +0200

    docker: add centos-8 and arch bootstrapped images

---
+++ dockerfiles/sf-arch/Dockerfile.build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SUPPORT_FIRECLOUD_DIR=/support-firecloud
+source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/common.inc.sh
+
+# DEPS
+[[ -f /support-firecloud.bootstrapped ]] || {
+    set -x
+    pacman_update
+
+
+    pacman_install_one ca-certificates
+    pacman_install_one gnupg
+
+    pacman_install_one git
+    pacman_install_one openssl
+    # pacman_install_one base-devel
+    pacman_install_one sudo
+    pacman_install_one which
+
+    source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/gitconfig.inc.sh
+    source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/user.inc.sh
+}
+
+source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/bootstrap.inc.sh
+source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/clean.inc.sh
