fe5af8b6abc81111dc2ec7f4845d93884e3347b8
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-27 10:41:58 +0200

    debug

--- ci/script.inc.sh
+++ ci/script.inc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 function sf_ci_run_script() {
+    sf_ci_debug
     # skip running 'make deps' again (part of 'sf_ci_run_install' already)
     # make all test
     make build check test
